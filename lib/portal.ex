437774efb8ca80128d1983fbd1260dddc10c7147
Rodrigo Martins <rrmartinsjg@gmail.com>
2016-10-29 21:25:41 -0200

    changes with new function

--- lib/portal.ex
+++ lib/portal.ex
@@ -3,18 +3,24 @@ defmodule Portal do
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
+  # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+  # for other strategies and supported options
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
+      worker(Portal.Door, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Shoots a new door with the given `color`.
+  """
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 
   defstruct [:left, :right]
